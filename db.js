ea091a550bffd038924ead9fad5fc8158d75a245
Larry Jung <jhj46456@gmail.com>
2019-04-11 15:48:14 +0900

    update

--- db.js
+++ db.js
@@ -1,50 +1,50 @@
-const videos = [
+export const videos = [
     {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views : 24,
-        videoFile : "https://archive.org/details/BigBuckBunny_124",
-        creator : {
-            id : 121212,
-            name : "Larry",
-            email : "jhj46456@gmail.com"
-        }
+      id: 324393,
+      title: "Video awesome",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
     },
     {
-        id: 324456,
-        title: "Video dadw",
-        description: "This is something I love",
-        views : 24,
-        videoFile : "https://archive.org/details/BigBuckBunny_124",
-        creator : {
-            id : 121212,
-            name : "Larry",
-            email : "jhj46456@gmail.com"
-        }
+      id: 1212121,
+      title: "Video super",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
     },
     {
-        id: 324123,
-        title: "Video awedawdaw",
-        description: "This is something I love",
-        views : 24,
-        videoFile : "https://archive.org/details/BigBuckBunny_124",
-        creator : {
-            id : 121212,
-            name : "Larry",
-            email : "jhj46456@gmail.com"
-        }
+      id: 55555,
+      title: "Video nice",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
     },
     {
-        id: 324553,
-        title: "Video awedawdaws",
-        description: "This is something I love",
-        views : 24,
-        videoFile : "https://archive.org/details/BigBuckBunny_124",
-        creator : {
-            id : 121212,
-            name : "Larry",
-            email : "jhj46456@gmail.com"
-        }
+      id: 11111,
+      title: "Video perfect",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
     }
-]
+  ];
