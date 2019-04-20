f4fa4f0d7f332cf926b17bd5cee744b2b60b3479
Larry Jung <jhj46456@gmail.com>
2019-04-20 21:22:44 +0900

    facebook auth 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
+import { access } from "fs";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
@@ -61,6 +62,21 @@ export const githubLoginCallback = async (_, __, profile, cb) => {
   }
 };
 
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postFacebookLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const postGithubLogIn = (req, res) => {
   res.redirect(routes.home);
 };
@@ -74,8 +90,17 @@ export const getMe = (req, res) => {
   res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
 
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
