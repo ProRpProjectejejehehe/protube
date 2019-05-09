8284f66f797b79fa78c95d138dba24619e169e41
Larry Jung <jhj46456@gmail.com>
2019-05-09 21:27:47 +0900

    bug fix

--- assets/js/delComment.js
+++ assets/js/delComment.js
@@ -1,30 +1,46 @@
 import axios from "axios";
 
+const commentList = document.getElementById("jsCommentList");
 const commentNumber = document.getElementById("jsCommentNumber");
-
-const delBtn = document.getElementById("js-commentDelete");
+const delBtn = document.querySelectorAll(".comment__delete");
 
 const decreaseNumber = () => {
   commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) - 1;
 };
 
+const delComment = (id, target) => {
+  const span = target.parentElement;
+  const li = span.parentElement;
+  commentList.removeChild(li);
+  decreaseNumber();
+};
+
 const handleRemoveComment = async event => {
-  const target = event.target.parentNode;
+  const target = event.target;
   console.log(target);
-  const commentId = event.target.id;
+  const commentId = target.id;
   console.log(commentId);
   const response = await axios({
     url: `/api/comment/${commentId}/remove`,
-    method: "POST"
+    method: "POST",
+    data: { commentId }
   });
   if (response.status === 200) {
-    target.parentNode.remove();
-    decreaseNumber();
+    delComment(commentId, target);
   }
 };
 
+const addEvent = () => {
+  delBtn.forEach(function(btn) {
+    const Btn = btn;
+    Btn.addEventListener("click", handleRemoveComment);
+  });
+};
+
 function init() {
-  delBtn.addEventListener("click", handleRemoveComment);
+  addEvent();
 }
 
-init();
+if (delBtn) {
+  init();
+}
